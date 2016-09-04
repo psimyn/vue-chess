b34116255fb626ae4da605bea7bbc5b801c79d43
psimyn <psimyn@gmail.com>
2016-09-05 09:16:09 +1000

    fix minify

--- webpack.config.js
+++ webpack.config.js
@@ -45,17 +45,17 @@ module.exports = {
 if (process.env.NODE_ENV === 'production') {
   module.exports.output.publicPath = '/vue-chess/dist/'
   module.exports.devtool = '#source-map'
-  // // http://vue-loader.vuejs.org/en/workflow/production.html
-  // module.exports.plugins = (module.exports.plugins || []).concat([
-  //   new webpack.DefinePlugin({
-  //     'process.env': {
-  //       NODE_ENV: '"production"'
-  //     }
-  //   }),
-  //   // new webpack.optimize.UglifyJsPlugin({
-  //   //   compress: {
-  //   //     warnings: false,
-  //   //   }
-  //   // })
-  // ])
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+      }
+    })
+  ])
 }
