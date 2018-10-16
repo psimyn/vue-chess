3df7e105b8333354fc4836b39134015d0226956e
psimyn <psimyn@gmail.com>
2018-10-16 20:04:51 +1100

    Split Tabs into separate component

--- src/App.vue
+++ src/App.vue
@@ -7,58 +7,15 @@
           <board />
         </transition>
 
-        <div
-          class="collapse"
-          :class="{ expanded }"
-        >
-          <el-button
-            type="text"
-            round
-            class="float-right"
-            :icon="expandIcon"
-            @click="toggleExpanded"
-          />
-
-          <el-tabs
-            type="border-card"
-            class="chess-tabs"
-          >
-            <el-tab-pane label="Play">
-              <status />
-              <captured-pieces />
-            </el-tab-pane>
-            <el-tab-pane label="Analyze">
-              <move-history></move-history>
-            </el-tab-pane>
-            <el-tab-pane
-              label="My Games"
-              lazy
-            >
-              <game-list />
-            </el-tab-pane>
-            <el-tab-pane
-              label="Settings"
-              lazy
-            >
-              <player />
-              <h3>About</h3>
-              Version: {{ version }}
-            </el-tab-pane>
-          </el-tabs>
-        </div>
+        <collapse-tabs />
       </div>
     </div>
   </template>
 
 <script>
 import Board from "./components/Board.vue";
-import CapturedPieces from "./components/CapturedPieces.vue";
-import Status from "./components/Status.vue";
-import Player from "./components/Player.vue";
-import GameList from "./components/GameList.vue";
-import MoveHistory from "./components/MoveHistory.vue";
-import Loader from "./components/Loader.vue";
-import { mapActions, mapGetters, mapState } from "vuex";
+import CollapseTabs from "./components/CollapseTabs.vue";
+import { mapActions, mapGetters } from "vuex";
 import querystring from "querystring";
 
 function generateGameId() {
@@ -68,38 +25,19 @@ function generateGameId() {
 export default {
   components: {
     Board,
-    CapturedPieces,
-    GameList,
-    Loader,
-    MoveHistory,
-    Player,
-    Status
-  },
-  data() {
-    return {
-      expanded: false
-    };
+    CollapseTabs
   },
   computed: {
-    ...mapState({
-      version: "version"
-    }),
     ...mapGetters({
       player: "player",
       game: "game",
       gameId: "gameId",
       moves: "moves",
       loading: "loading"
-    }),
-    expandIcon() {
-      return this.expanded ? "el-icon-arrow-down" : "el-icon-arrow-up";
-    }
+    })
   },
   methods: {
-    ...mapActions(["loadGame", "addMoves"]),
-    toggleExpanded() {
-      this.expanded = !this.expanded;
-    }
+    ...mapActions(["loadGame", "addMoves"])
   },
   mounted() {
     let isNewGame;
@@ -121,7 +59,7 @@ export default {
 };
 </script>
 
-  <style>
+<style>
 body {
   font-family: "Roboto", sans-serif;
   margin: 0;
@@ -150,16 +88,6 @@ body * {
   display: flex;
 }
 
-.collapse {
-  position: relative;
-}
-
-.float-right {
-  position: absolute;
-  right: 0;
-  z-index: 1;
-}
-
 button {
   border: solid 1px #222;
   border-bottom: solid 2px #888;
@@ -175,36 +103,5 @@ button {
 .slide-leave-to {
   opacity: 0;
   transform: translate3d(0, -40%, 0);
-}
-
-.collapse {
-  margin-top: 0;
-  transition: max-height 0.1s ease-out, transform 0.1s ease-out;
-  z-index: 7;
-  position: absolute;
-  width: 100%;
-}
-
-.el-tabs__content {
-  overflow: auto;
-  min-height: 2em;
-}
-
-.expanded {
-  position: absolute;
-  transform: translateY(calc(-100vmin + 40px));
-  left: 0;
-  right: 0;
-  z-index: 7;
-}
-
-@media (min-width: 600px) and (min-height: 600px) {
-  .expanded {
-    transform: translateY(calc(-600px));
-  }
-}
-
-.expanded .el-tabs {
-  min-height: 100vh;
 }
 </style>
---
+++ src/components/CollapseTabs.vue
@@ -0,0 +1,128 @@
+<template>
+  <div
+    class="collapse"
+    :class="{ expanded }"
+  >
+    <el-button
+      type="text"
+      round
+      class="float-right"
+      :icon="expandIcon"
+      @click="toggleExpanded"
+    />
+
+    <el-tabs
+      type="border-card"
+      class="chess-tabs"
+    >
+      <el-tab-pane label="Play">
+        <status />
+        <captured-pieces />
+      </el-tab-pane>
+      <el-tab-pane label="Analyze">
+        <move-history></move-history>
+      </el-tab-pane>
+      <el-tab-pane
+        label="My Games"
+        lazy
+      >
+        <game-list />
+      </el-tab-pane>
+      <el-tab-pane
+        label="Settings"
+        lazy
+      >
+        <player />
+        <h3>About</h3>
+        Version: {{ version }}
+      </el-tab-pane>
+    </el-tabs>
+  </div>
+</template>
+
+<script>
+import CapturedPieces from "./CapturedPieces.vue";
+import Status from "./Status.vue";
+import Player from "./Player.vue";
+import GameList from "./GameList.vue";
+import MoveHistory from "./MoveHistory.vue";
+import Loader from "./Loader.vue";
+import { mapActions, mapGetters, mapState } from "vuex";
+
+export default {
+  components: {
+    CapturedPieces,
+    GameList,
+    Loader,
+    MoveHistory,
+    Player,
+    Status
+  },
+  data() {
+    return {
+      expanded: false
+    };
+  },
+  computed: {
+    ...mapState({
+      version: "version"
+    }),
+    expandIcon() {
+      return this.expanded ? "el-icon-arrow-down" : "el-icon-arrow-up";
+    }
+  },
+  methods: {
+    toggleExpanded() {
+      this.expanded = !this.expanded;
+    }
+  }
+};
+</script>
+
+<style>
+.collapse {
+  position: relative;
+}
+
+.float-right {
+  position: absolute;
+  right: 0;
+  z-index: 1;
+}
+
+.collapse {
+  margin-top: 0;
+  transition: min-height 0.1s ease-out 0.1s, transform 0.1s ease-out;
+  z-index: 7;
+  position: absolute;
+  width: 100%;
+}
+
+.el-tabs__content {
+  overflow: auto;
+}
+
+.expanded {
+  position: absolute;
+  transform: translateY(calc(-100vmin + 40px));
+  left: 0;
+  right: 0;
+  z-index: 7;
+}
+
+@media (min-width: 600px) and (min-height: 600px) {
+  .expanded {
+    transform: translateY(calc(-600px));
+  }
+}
+
+.el-tabs {
+  transition: min-height 0.1s ease-out 0.1s;
+  min-height: 8em;
+}
+
+.expanded .el-tabs {
+  transition: none;
+  min-height: 100vh;
+}
+</style>
